8ae125fdc25989cc3bb00f91d86a42c2d5047139
Hasanat902 <abulhasanat902@gmail.com>
2023-01-22 12:02:51 +0600

    load appointment options from database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,13 +12,27 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.nfnms.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nfnms.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+
+        const appointmentOptionCollection = client.db('patientManagement').collection('appointmentOptions');
+
+
+        app.get('/appointmentOptions', async(req, res) => {
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 
 
